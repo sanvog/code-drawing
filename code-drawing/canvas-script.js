8be7b2db78d9650e4f163f16d2169b8b0f6864d7
Sanna <70701032+sanvog@users.noreply.github.com>
2022-04-05 20:39:55 +0200

    Cleaning

--- code-drawing/canvas-script.js
+++ code-drawing/canvas-script.js
@@ -4,37 +4,28 @@ window.onload = function () {
 let canvas = document.getElementById("rectangle-canvas");
 let context = canvas.getContext("2d");
 
-//Green rectangle
-context.fillStyle = "green";//Creates green brush
-context.fillRect(10,40,40,80);//fills rectangle, 2 coordinate, 1 width, 1 height.
+context.fillStyle = "green";
+context.fillRect(10,40,40,80);
 
-//Purple rectangle
-context.fillStyle = "purple";//selects color of brush
+context.fillStyle = "purple";
 context.fillRect(30,70,40,80);
 
-//Draw line steps
-context.beginPath();//starting, reset the context state.
-context.strokeStyle = "red"; //red brush color.
-context.lineWidth = 10; //width of stroke.
-context.moveTo(30, 70); //moveTo(x,y) starting point of line.
-context.lineTo(130, 70); //line(x,y) endpoint of the line.
-//draw the line, calling the function:
+context.beginPath();
+context.strokeStyle = "red"; 
+context.lineWidth = 10; 
+context.moveTo(30, 70); 
+context.lineTo(130, 70); 
 context.stroke();
 
-//Draw zick-zack line
-context.beginPath();//starting, reset the context state.
-context.strokeStyle = "purple"; //red brush color.
-context.lineWidth = 5; //width of stroke.
-context.moveTo(15, 10); //moveTo(x,y) starting point of line.
-context.lineTo(40, 35); //line(x,y) endpoint of the line.
+context.beginPath();
+context.strokeStyle = "purple"; 
+context.lineWidth = 5; 
+context.moveTo(15, 10); 
+context.lineTo(40, 35); 
 context.lineTo(65, 10);
 context.lineTo(90, 35);
 context.lineTo(115, 10);
-//draw the line, calling the function:
 context.stroke();
-
-
-
 
 }
 
